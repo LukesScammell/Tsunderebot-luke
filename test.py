415d7c3838dad4ac77ec99356b2f63409e61d20a
LukesScammell <luke-scammell@live.nmit.ac.nz>
2025-08-10 14:16:39 +1200

    fixing everything

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Testing one two three")
-engine.runAndWait()
+from TTS.api import TTS
+import tempfile
+import pygame
+import os
+
+tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC")
+
+with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_wav:
+    tts.tts_to_file(text="Hello world!", file_path=temp_wav.name)
+    temp_path = temp_wav.name
+
+pygame.mixer.init()
+pygame.mixer.music.load(temp_path)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+os.remove(temp_path)
